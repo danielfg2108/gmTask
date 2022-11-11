9ca1039cc82177f507d66d0154c1250c8affac9c
Jafet Daniel Fonseca Garcia <danielfg2108@gmail.com>
2022-11-11 16:32:05 -0600

    11/11/2022
    11/11/2022

--- tareas/tareas.php
+++ tareas/tareas.php
@@ -1,10 +1,7 @@
 <?php require_once '../header.php'; ?>
 <?php
-$sql = "SELECT * FROM tareas WHERE id_usuario='$id'"; //generar consulta
-$resultado = $mysqli->query($sql); //guardar consulta
-
-$sql_colaboradores = "SELECT * FROM colaboradores_tareas WHERE id_usuario='$id'"; //generar consulta colaboradores
-$resultado_colaboradores = $mysqli->query($sql_colaboradores); //guardar consulta proyectos
+$sql_colaboradores = "SELECT id_tarea FROM colaboradores_tareas WHERE id_usuario='$id'"; //generar consulta colaboradores
+$resultado_colaboradores = $mysqli->query($sql_colaboradores); //guardar consulta
 ?>
 <!-- Autor: Jafet Daniel Fonseca Garcia -->
 <h1 id="saludo" class="mt-4">Mis Tareas</h1>
@@ -32,88 +29,34 @@ $resultado_colaboradores = $mysqli->query($sql_colaboradores); //guardar consult
                 <tr>
                     <th>Ver</th>
                     <th>Nombre</th>
-                    <th>Colaboradores</th>
+                    <th>Descripción</th>
                     <th>Fecha de Entrega</th>
                     <th>Proyecto Asignado</th>
+                    <th>Status</th>
                 </tr>
             </thead>
             <tfoot>
                 <tr>
                     <th>Ver</th>
                     <th>Nombre</th>
-                    <th>Colaboradores</th>
+                    <th>Descripción</th>
                     <th>Fecha de Entrega</th>
                     <th>Proyecto Asignado</th>
+                    <th>Status</th>
                 </tr>
             </tfoot>
-            <tbody class="table-dark">
-                <?php //tareas creadas por el usuario
-                while ($row = mysqli_fetch_array($resultado)) {
-                                $id_tarea = $row['id_tarea'];
-                                $sql_colaboradores = "SELECT colaboradores_tareas.id_usuario, usuarios.nombre, usuarios.apellidos 
-                                                      FROM colaboradores_tareas
-                                                      NATURAL JOIN usuarios
-                                                      WHERE colaboradores_tareas.id_tarea='$id_tarea' "; //generar consulta
-
-                                $resultado_colaboradores = $mysqli->query($sql_colaboradores); //guardar consulta
-                                $num_colaboradores = $resultado_colaboradores->num_rows; //si la consulta genero resultados
-                ?>
-                    <tr>
-                        <td style="width: 1px;">
-                            <a type="button" href="detalles_tarea.php?id_tarea=<?php echo $row['id_tarea']?>" class="btn btn-primary"><i class="fa-solid fa-eye"></i></a>
-                        </td>
-                        <td><?php echo $row['nombre'] ?></td>
-
-                        <td>
-                            <?php
-                            while ($row_colaboradores = mysqli_fetch_array($resultado_colaboradores)) {                               
-                            ?>
-                            <span><?php echo $row_colaboradores['nombre'] ?> <?php echo $row_colaboradores['apellidos'] ?></span>
-                            <br>
-                            <?php                                                      
-                            }
-                            ?>
-                           
-                        </td>
-
-                        <td><?php echo $row['fecha_entrega'] ?></td>
-                        <?php
-                        $id_t = $row['id_tarea'];
-                        $sql_t = "SELECT id_proyecto FROM proyectos_tareas WHERE id_tarea='$id_t'"; //generar consulta
-                        $resultado_t = $mysqli->query($sql_t); //guardar consulta
-                        $row_t = mysqli_fetch_array($resultado_t); //ejecutar consulta (fetch devuelve un solo registro)
-                        $num = $resultado_t->num_rows; //si la consulta genero resultados
-
-                        if ($num > 0) {
-                            $id_p = $row_t['id_proyecto'];
-                            $sql_p = "SELECT nombre FROM proyectos WHERE id_proyecto='$id_p'"; //generar consulta
-                            $resultado_p = $mysqli->query($sql_p); //guardar consulta
-                            $row_p = mysqli_fetch_array($resultado_p); //ejecutar consulta (fetch devuelve un solo registro)
-                        ?>
-                            <td><?php echo $row_p['nombre'] ?></td>
-                        <?php
-                        } else {
-                        ?>
-                        <td>SIN PROYECTO</td>
-                        <?php
-                        }
-                        ?>
-                    </tr>
-                <?php
-                }
-                ?>
-
-               <?php //colaborador de tareas
+            <tbody class="table-dark">           
+               <?php
                 while ($row_colaboradores = mysqli_fetch_array($resultado_colaboradores)) {
 
-                    $id_t_colaborador = $row_colaboradores['id_tarea']; //guardar id en variable
+                    $id_t = $row_colaboradores['id_tarea']; //guardar id en variable
                
-                    $sql_t_colaborador = "SELECT * FROM tareas WHERE id_tarea='$id_t_colaborador'"; //consulta para obtener los datos de la tarea
+                    $sql_t_colaborador = "SELECT * FROM tareas WHERE id_tarea='$id_t'"; //consulta para obtener los datos de la tarea
                     $resultado_t_colaborador = $mysqli->query($sql_t_colaborador); //guardar consulta
                     $row_t_colaborador = mysqli_fetch_array($resultado_t_colaborador); //ejecutar consulta (fetch devuelve un solo registro)
                     $num_t_colaborador = $resultado_t_colaborador->num_rows; //si la consulta genero resultados  
     
-                    if ( ($num_t_colaborador > 0) && ($row_t_colaborador['id_usuario'] != $id) ) { 
+                    if ($num_t_colaborador > 0) { 
                 ?>
                     <tr>
                         <td style="width: 1px;">
@@ -144,12 +87,12 @@ $resultado_colaboradores = $mysqli->query($sql_colaboradores); //guardar consult
                         <?php
                         }
                         ?>
+                         <td><?php echo $row_t_colaborador['status']?></td>
                     </tr>
                 <?php
                     } //if
                 } //while
                 ?>
-
             </tbody>
         </table>
     </div>
